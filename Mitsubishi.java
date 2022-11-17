05d62fe5040baa05f337d45ee77c60a0111d5f13
Dusa Sai Kiran <110330338+Kirankruze@users.noreply.github.com>
2022-11-17 12:46:58 +0530

    Add files via upload

---
+++ Mitsubishi.java
@@ -0,0 +1,18 @@
+public class Mitsubishi extends Car {
+   
+   public Mitsubishi(int cylinders,String name){
+       super(cylinders,name);
+   }
+   @Override
+   public  String startEngine(){
+        return "Mitsubishi -> the car's engine is starting";
+    }
+    @Override
+    public String accelerate(){
+        return "Mitsubishi ->the car is accelerating";
+    }
+    @Override
+    public String brake(){
+        return "Mitsubishi ->the car is braking";
+    }
+}
